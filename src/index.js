6ceeb4c24b8c5365cf19969d26bb9f37faf25947
tamara-doe <116146682+tamara-doe@users.noreply.github.com>
2022-10-27 09:12:23 +0200

    Display the forecast data with the API

--- src/index.js
+++ src/index.js
@@ -21,28 +21,45 @@ function formatDate(timestamp) {
   return `Last updated: ${day} ${hours}:${minutes}`;
 }
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[day];
+}
+
 function displayForecast(response) {
-  console.log(response);
+  let forecast = response.data.daily;
+  console.log(response.data.daily);
 
   let forecastElement = document.querySelector("#forecast");
   let forecastHTML = `<div class="row">`;
-  let days = ["Thu", "Fri", "Sat", "Sun"];
-  days.forEach(function (day) {
-    forecastHTML =
-      forecastHTML +
-      `
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        `
     <div class="col-2">
-      <div class="forecast-date">${day}</div>
+      <div class="forecast-date">${formatDay(forecastDay.dt)}</div>
+      
       <img
-        src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
+        src= "http://openweathermap.org/img/wn/${
+          forecastDay.weather[0].icon
+        }@2x.png"
         alt=""
         width="36"
       />
       <div class="forecast-temperature">
-        <span class="forecast-temperature-max">18°</span>
-        <span class="forecast-temperature-min">5°</span>
+        <span class="forecast-temperature-max">${Math.round(
+          forecastDay.temp.max
+        )}°</span>
+        <span class="forecast-temperature-min">${Math.round(
+          forecastDay.temp.min
+        )}°</span>
       </div>
     </div>`;
+    }
   });
   forecastHTML = forecastHTML + `</div>`;
   forecastElement.innerHTML = forecastHTML;
@@ -125,8 +142,6 @@ function displayCelsius(event) {
 }
 
 let celsiusTemperature = null;
-
-displayForecast();
 
 let searchForm = document.querySelector("#search-form");
 searchForm.addEventListener("submit", handlesubmit);
